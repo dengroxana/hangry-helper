1abe778e97fcdb15120036c9fcdf435150a1fdc8
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2020-05-16 01:45:58 -0400

    changed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.destroy_all
 Category.destroy_all
 Restaurant.destroy_all
 
-admin = User.create(email: "admin@email.com", password: "123456")
+admin = User.create(first_name: "John", last_name: "Smith", email: "admin@email.com", password: "123456")
 
 p "#{User.count} user(s) created"
 
@@ -23,28 +23,28 @@ desserts = Category.create(category_name: "Desserts")
 
 p "#{Category.count} categories created"
 
-ichiran = Restaurant.create(restaurant_name: "Ichiran", category: asian, user: admin)
-up_thai = Restaurant.create(restaurant_name: "Up Thai", category: asian, user: admin)
-bocaphe = Restaurant.create(restaurant_name: "BoCaphe", category: asian, user: admin)
+ichiran = Restaurant.create(restaurant_name: "Ichiran", price_range: "$$", location: "374 Johnson Ave Brooklyn, NY 11206", category: asian, user: admin)
+up_thai = Restaurant.create(restaurant_name: "Up Thai", price_range: "$$", location: "1411 2nd Ave New York, NY 10021", category: asian, user: admin)
+bocaphe = Restaurant.create(restaurant_name: "BoCaphe", price_range: "$$", location: "222 Lafayette St New York, NY 10012", category: asian, user: admin)
 
-empanada_mama = Restaurant.create(restaurant_name: "Empanada Mama", category: latin, user: admin)
-cafe_habana = Restaurant.create(restaurant_name: "Cafe Habana", category: latin, user: admin)
-calle_ocho = Restaurant.create(restaurant_name: "Calle Ocho", category: latin, user: admin)
+empanada_mama = Restaurant.create(restaurant_name: "Empanada Mama", price_range: "$", location: "95 Allen St New York, NY 10002", category: latin, user: admin)
+cafe_habana = Restaurant.create(restaurant_name: "Cafe Habana", price_range: "$$", location: "17 Prince St New York, NY 10012", category: latin, user: admin)
+calle_ocho = Restaurant.create(restaurant_name: "Calle Ocho", price_range: "$$", location: "2756 Broadway New York, NY 10025", category: latin, user: admin)
 
-veselka = Restaurant.create(restaurant_name: "Veselka", category: european, user: admin)
-cafe_katja = Restaurant.create(restaurant_name: "Cafe Katja", category: european, user: admin)
-aquavit = Restaurant.create(restaurant_name: "Aquavit", category: european, user: admin)
+veselka = Restaurant.create(restaurant_name: "Veselka", price_range: "$$", location: "144 2nd Ave New York, NY 10003", category: european, user: admin)
+cafe_katja = Restaurant.create(restaurant_name: "Cafe Katja", price_range: "$$", location: "79 Orchard St New York, NY 10002", category: european, user: admin)
+aquavit = Restaurant.create(restaurant_name: "Aquavit", price_range: "$$$$", location: "65 E 55th St New York, NY 10022", category: european, user: admin)
 
-jane = Restaurant.create(restaurant_name: "Jane", category: american, user: admin)
-the_smith = Restaurant.create(restaurant_name: "The Smith", category: american, user: admin)
-friedmans = Restaurant.create(restaurant_name: "Friedman's", category: american, user: admin)
+jane = Restaurant.create(restaurant_name: "Jane", price_range: "$$", location: "100 W Houston St New York, NY 10012", category: american, user: admin)
+the_smith = Restaurant.create(restaurant_name: "The Smith", price_range: "$$", location: "55 3rd Ave New York, NY 10003", category: american, user: admin)
+friedmans = Restaurant.create(restaurant_name: "Friedman's", price_range: "$$", location: "132 W 31st St New York, NY 10001", category: american, user: admin)
 
-cafe_mogador = Restaurant.create(restaurant_name: "Cafe Mogador", category: middle_eastern, user: admin)
-taboonette = Restaurant.create(restaurant_name: "Taboonette", category: middle_eastern, user:admin)
-tanoreen = Restaurant.create(restaurant_name: "Tanoreen", category:middle_eastern, user:admin)
+cafe_mogador = Restaurant.create(restaurant_name: "Cafe Mogador", price_range: "$$", location: "101 St Marks Pl New York, NY 10009", category: middle_eastern, user: admin)
+taboonette = Restaurant.create(restaurant_name: "Taboonette", price_range: "$$", location: "30 E 13th St New York, NY 10003", category: middle_eastern, user: admin)
+tanoreen = Restaurant.create(restaurant_name: "Tanoreen", price_range: "$$", location: "7523 3rd Ave Brooklyn, NY 11209", category: middle_eastern, user: admin)
 
-levain_bakery = Restaurant.create(restaurant_name: "Levain Bakery",category:desserts, user:admin )
-spot_dessert_bar = Restaurant.create(restaurant_name: "Spot Dessert Bar", category:desserts, user:admin)
-taiyaki_nyc = Restaurant.create(restaurant_name: "Taiyaki NYC",category:desserts, user:admin)
+levain_bakery = Restaurant.create(restaurant_name: "Levain Bakery", price_range: "$$", location: "1484 3rd Ave New York, NY 10023", category: desserts, user: admin)
+spot_dessert_bar = Restaurant.create(restaurant_name: "Spot Dessert Bar", price_range: "$$", location: "13 St Marks Pl New York, NY 10003", category: desserts, user: admin)
+taiyaki_nyc = Restaurant.create(restaurant_name: "Taiyaki NYC", price_range: "$", location: "119 Baxter St New York, NY 10013", category: desserts, user: admin)
 
 p "#{Restaurant.count} restaurants created"
